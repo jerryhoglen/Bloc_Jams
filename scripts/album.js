1996fa2731555dc74d7dab61892aa418ae675671
Jerry Hoglen <jerryhoglen@me.com>
2017-11-18 22:09:11 -0500

    "Added play button"

--- scripts/album.js
+++ scripts/album.js
@@ -91,7 +91,7 @@ var albumJerry = {
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">' +
-    '  <td class="song-item-number">' + songNumber + '</td>' +
+    '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' +
     '  <td class="song-item-title">' + songName + '</td>' +
     '  <td class="song-item-duration">' + songLength + '</td>' +
     '</tr>';
@@ -124,17 +124,31 @@ var setCurrentAlbum = function(album) {
     albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
   }
 };
-
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
 
-  var albums = [albumPicasso, albumMarconi, albumJerry];
-  var index = 1;
-  albumImage.addEventListener("click", function(event) {
-    setCurrentAlbum(albums[index]);
-    index++;
-    if (index == albums.length) {
-      index = 0;
+  songListContainer.addEventListener('mouseover', function(event) {
+    if (event.target.parentElement.className === 'album-view-song-item') {
+      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
     }
   });
-};
+
+  for (var i = 0; i < songRows.length; i++) {
+    songRows[i].addEventListener('mouseleave', function(event) {
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    });
+  }
+}
+
+var albums = [albumPicasso, albumMarconi, albumJerry];
+var index = 1;
+albumImage.addEventListener("click", function(event) {
+  setCurrentAlbum(albums[index]);
+  index++;
+  if (index == albums.length) {
+    index = 0;
+  }
+});
